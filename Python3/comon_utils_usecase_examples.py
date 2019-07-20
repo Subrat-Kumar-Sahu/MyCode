608b9be69b0f0b487a451b90c6c6bd4e6ccb3e47
Subrat Kumar <50540433+Subrat-Kumar-Sahu@users.noreply.github.com>
2019-07-20 18:32:15 +0530

    Add files via upload

--- Python3/comon_utils_usecase_examples.py
+++ Python3/comon_utils_usecase_examples.py
@@ -1,13 +1,13 @@
 #!/home/cloudera/anaconda3/bin/python
 
-import logging
 import sys
+import logging
 
 sys.path.append('/apps/comm_fn_pkg')
 import comon_utils as cu
 
 def main():
-    cu.setLogger(cu.getCommonPath('/apps/conf', 'common_path.txt', 'config_path', 'LOG_DIR'), __file__)
+    cu.set_std_logger(logging.DEBUG, cu.get_std_apps_env('/apps/conf', 'common_env_var.txt', 'config_path', 'LOG_DIR').strip('"'), __file__)
 
     logging.info('Started')
 
@@ -17,16 +17,22 @@ def main():
     logging.error('This is an error message')
     logging.critical('This is a critical message')
 
-    checkLogging()
+    check_logging()
+    check_env_setting()
 
     logging.info('Finished')
 
-def checkLogging():
+def check_logging():
     logging.debug('This is a debug message')
     logging.info('This is an info message')
     logging.warning('This is a warning message')
     logging.error('This is an error message')
     logging.critical('This is a critical message')
+
+def check_env_setting():
+    STD_APPS_ENV_OBJ = cu.SetStdAppsEnv('/apps/conf', 'common_env_var.txt')
+    logging.info('IP_DIR: ' + STD_APPS_ENV_OBJ.IP_DIR)
+    
 
 if __name__ == '__main__':
     main()
