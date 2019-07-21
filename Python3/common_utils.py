f61f06be194d67facf43e1cbd9e6e146c138cc47
Subrat Kumar <50540433+Subrat-Kumar-Sahu@users.noreply.github.com>
2019-07-21 12:29:54 +0530

    Add files via upload

---
+++ Python3/common_utils.py
@@ -0,0 +1,47 @@
+#!/home/cloudera/anaconda3/bin/python
+
+#sys.path.append('/apps/comm_fn_pkg')
+
+import os
+import re
+import sys
+import logging
+import datetime
+import configparser as cp
+
+class SetStdAppsEnv():
+    def __init__(self, p_path, p_conf_file):
+
+        self.FILENAME = p_path + '/' + p_conf_file
+
+        with open(self.FILENAME, 'r') as fh:
+            for LINE in fh:
+                if re.search("(?=^[^'(#|[)'])(?=^[^\s*$])", LINE):
+                    exec('self.' + LINE)
+
+def get_std_apps_env(p_path, p_conf_file, p_section_name, p_parameter_name):
+    FILE_NAME = p_path + '/' + p_conf_file
+
+    parser = cp.ConfigParser()
+    parser.read(FILE_NAME)
+    return parser.get(p_section_name, p_parameter_name)
+
+def set_std_logger(p_log_level, p_log_file):
+    logging.basicConfig(
+                        level = p_log_level,
+                        format = '%(asctime)s:%(filename)s:%(levelname)-8s:%(message)s',
+                        datefmt = '%Y-%m-%d %H:%M:%S',
+                        handlers = [logging.FileHandler(p_log_file, 'w'), logging.StreamHandler()]
+                       )
+
+def set_custom_logger(p_log_level, p_log_path, p_script_name):
+    global LOG_FILE
+
+    LOG_FILE=p_log_path + '/' + os.path.splitext(os.path.basename(p_script_name))[0] + '_' + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + '.log'
+
+    logging.basicConfig(
+                        level = p_log_level,
+                        format = '%(asctime)s:%(filename)s:%(levelname)-8s:%(message)s',
+                        datefmt = '%Y-%m-%d %H:%M:%S',
+                        handlers = [logging.FileHandler(LOG_FILE, 'w'), logging.StreamHandler()]
+                       )
