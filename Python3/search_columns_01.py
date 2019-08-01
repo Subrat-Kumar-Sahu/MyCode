e1676985c4b6bfd7aeefd03ac4aca35fe8ba0c01
Subrat Kumar <50540433+Subrat-Kumar-Sahu@users.noreply.github.com>
2019-08-01 07:56:11 +0530

    Add files via upload

---
+++ Python3/search_columns_01.py
@@ -0,0 +1,59 @@
+#!/home/cloudera/anaconda3/bin/python
+
+####################################################################################################
+# Description: This is used to search column name in a table based on a particular pattern.
+#
+# History:
+#        2019-08-01 : Subrat Kumar       : Created
+####################################################################################################
+
+import argparse
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import collect_list, concat, col, lit
+
+def search_col(p_db, p_table, p_search_str):
+    trg_db = p_db
+    trg_table = p_table
+    search_str = p_search_str
+
+    sql_stmt = "desc {0}.{1}".format(trg_db, trg_table)
+    col_df = spark.sql(sql_stmt).select("col_name", concat(lit("nvl("), "col_name", lit(", 0)")).alias("d_col_name"))
+
+    col_list = col_df.select("col_name").select(collect_list("col_name")).collect()[0][0]
+    d_col_list = col_df.select("d_col_name").select(collect_list("d_col_name")).collect()[0][0]
+
+    col_str = "||':'||".join(col_list)
+    d_col_str = "||':'||".join(d_col_list)
+
+    sql_stmt = "select count(*) from {0}.{1} where instr({2}, ':{3}:') <> 0".format(trg_db, trg_table, d_col_str, search_str)
+    REC_COUNT = spark.sql(sql_stmt).collect()[0][0]
+
+    if REC_COUNT > 0:
+        for i in col_list:
+            sql_stmt = "select count(*) from {0}.{1} where {2} = '{3}'".format(trg_db, trg_table, i, search_str);
+            REC_KOUNT = spark.sql(sql_stmt).collect()[0][0]
+            if REC_KOUNT > 0:
+                print("{0}:{1}:{2}:{3}".format(trg_db, trg_table, i, search_str))
+    else:
+        print("String '{0}' not found in {1}.{2}".format(search_str, trg_db, trg_table))
+
+if __name__ == '__main__':
+    spark = SparkSession.builder.appName('search_str_Test').enableHiveSupport().getOrCreate()
+
+    # Initialize the parser
+    parser = argparse.ArgumentParser(
+        description="Search String"
+    )
+
+    # Add the parameters positional/optional
+    parser.add_argument('-d', '--db', help = "Database Name", type = str)
+    parser.add_argument('-t', '--table', help = "Table Name", type = str)
+    parser.add_argument('-s', '--search', help = "Search String", type = str)
+
+    args = parser.parse_args()
+    print("Argument Details: {0}".format(args))
+
+    if not args.db.strip() or not args.table.strip() or not args.search.strip():
+        print("None of the argument can be blank/null")
+    else:
+        search_col(args.db, args.table, args.search)
