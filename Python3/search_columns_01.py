e6e5c294f6d3e589bab7a5c677e9c0e3113df5f4
Subrat Kumar <50540433+Subrat-Kumar-Sahu@users.noreply.github.com>
2019-08-01 08:34:27 +0530

    Add files via upload

--- Python3/search_columns_01.py
+++ Python3/search_columns_01.py
@@ -7,15 +7,13 @@
 #        2019-08-01 : Subrat Kumar       : Created
 ####################################################################################################
 
+import logging
+import datetime
 import argparse
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import collect_list, concat, col, lit
 
-def search_col(p_db, p_table, p_search_str):
-    trg_db = p_db
-    trg_table = p_table
-    search_str = p_search_str
-
+def search_col(trg_db, trg_table, search_str):
     sql_stmt = "desc {0}.{1}".format(trg_db, trg_table)
     col_df = spark.sql(sql_stmt).select("col_name", concat(lit("nvl("), "col_name", lit(", 0)")).alias("d_col_name"))
 
@@ -28,16 +26,31 @@ def search_col(p_db, p_table, p_search_str):
     sql_stmt = "select count(*) from {0}.{1} where instr({2}, ':{3}:') <> 0".format(trg_db, trg_table, d_col_str, search_str)
     REC_COUNT = spark.sql(sql_stmt).collect()[0][0]
 
+    logging.info("Sql Query: {0}".format(sql_stmt))
+    logging.info("Search string '{0}' found in table {1}.{2}".format(search_str, trg_db, trg_table))
+
+    print(LN_T)
+
     if REC_COUNT > 0:
         for i in col_list:
-            sql_stmt = "select count(*) from {0}.{1} where {2} = '{3}'".format(trg_db, trg_table, i, search_str);
+            sql_stmt = "select count(*) from {0}.{1} where {2} = '{3}'".format(trg_db, trg_table, i, search_str)
             REC_KOUNT = spark.sql(sql_stmt).collect()[0][0]
             if REC_KOUNT > 0:
-                print("{0}:{1}:{2}:{3}".format(trg_db, trg_table, i, search_str))
+                logging.info("{0}:{1}:{2}:{3}".format(trg_db, trg_table, i, search_str))
     else:
-        print("String '{0}' not found in {1}.{2}".format(search_str, trg_db, trg_table))
+        logging.warning("String '{0}' not found in {1}.{2}".format(search_str, trg_db, trg_table))
+
+    print(LN_T)
 
 if __name__ == '__main__':
+    logging.basicConfig(
+                         level = logging.INFO,
+                         format = '%(asctime)s:%(filename)s:%(levelname)-8s:%(message)s',
+                         datefmt = '%Y-%m-%d %H:%M:%S'
+                       )
+
+    LN_T = "~"*100
+
     spark = SparkSession.builder.appName('search_str_Test').enableHiveSupport().getOrCreate()
 
     # Initialize the parser
@@ -51,9 +64,9 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--search', help = "Search String", type = str)
 
     args = parser.parse_args()
-    print("Argument Details: {0}".format(args))
+    logging.info("Argument Details: {0}".format(args))
 
     if not args.db.strip() or not args.table.strip() or not args.search.strip():
-        print("None of the argument can be blank/null")
+        logging.error("None of the argument can be blank/null")
     else:
         search_col(args.db, args.table, args.search)
